47b97c4462de5d12c66545cd05333de3cc65a914
Denzel <denzelhartshorn@gmail.com>
2019-09-28 14:33:17 -0700

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,117 @@
+let resumeData = {
+  imagebaseurl: "",
+  name: "Denzel Hartshorn",
+  role: "Full Stack Developer (Front & Back End)",
+  linkedinId: "https://www.linkedin.com/in/denzel-hartshorn-147302106/",
+  // skypeid: "",
+  roleDescription: "",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/feed/",
+      className: "fa fa-linkedin"
+    },
+    {
+      name: "github",
+      url: "https://github.com/denzelhartshorn",
+      className: "fa fa-github"
+    }
+  ],
+  aboutme:
+    "A self-motivated, creative and dedicated web developer with strong teamwork, collaboration, and communication skills.  Quick to learn new tools and systems with the ability to thrive in independent or collaborative work environments.  Strong qualifications in problem-solving, product management, sales, marketing, communication and leadership.  Currently building projects with the MERN stack.",
+  address: "Palo Alto, California",
+  website: "https://github.com/denzelhartshorn",
+  education: [
+    {
+      UniversityName: "UC Berkeley Full-Stack Coding Bootcamp",
+
+      YearOfPassing: "2019"
+    },
+    {
+      UniversityName: "Whittier College",
+      YearOfPassing: "2016"
+    }
+  ],
+  work: [
+    {
+      CompanyName: "Meal Planner",
+      specialization: "Software Engineer (Front End & Back End)",
+      MonthOfLeaving: "August",
+      YearOfLeaving: "2019",
+      Achievements:
+        "Architected, developed and fully deployed a full-stack app that has authentication.  This app will take user input, create a profile, and then give meal suggestions based on users input."
+    },
+    {
+      CompanyName: "CryptoBob",
+      specialization: "Software Engineer (Front End & Back End)",
+      MonthOfLeaving: "July",
+      YearOfLeaving: "2019",
+      Achievements:
+        "Architected, developed a full-stack app that tracks cryptoCurrency fluxuations as well as providing real time news"
+    },
+    {
+      CompanyName: "Berkely Bus App",
+      specialization: "Software Engineer (Front End & Back End)",
+      MonthOfLeaving: "June",
+      YearOfLeaving: "2019",
+      Achievements:
+        "Architected, developed a full-stack app that will help you find your destination, fastest bus route and nearest bus stop in Berkeley, CA."
+    }
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5"
+    },
+    {
+      skillname: "CSS"
+    },
+    {
+      skillname: "JavaScript"
+    },
+    {
+      skillname: "MongoDB-Mongoose"
+    },
+    {
+      skillname: "Express"
+    },
+    {
+      skillname: "Reactjs"
+    },
+    {
+      skillname: "Nodejs"
+    },
+    {
+      skillname: "GitHub-Lab"
+    },
+    {
+      skillname: "MYSQL-SQL"
+    }
+  ],
+  portfolio: [
+    {
+      name: "Meal Planner",
+      description: "mobileapp",
+      imgurl:
+        "https://camo.githubusercontent.com/9bbc9c35028ec68d8418b08ead09d6943a6bd5dc/68747470733a2f2f692e6c656e7364756d702e636f6d2f692f69696f4b426d2e706e67"
+    },
+    {
+      name: "CryptoBob",
+      description: "mobileapp",
+      imgurl:
+        "https://camo.githubusercontent.com/dd1affe2776d5dc054629e1eac5fa67264dc20de/68747470733a2f2f692e6c656e7364756d702e636f6d2f692f696a6270466b2e706e67"
+    },
+    {
+      name: "Berkely Bus App",
+      description: "mobileapp",
+      imgurl: "images/bba-screenshot.png"
+    },
+    {
+      name: "E-Commerce site using MERN",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg"
+    }
+  ]
+};
+
+export default resumeData;
