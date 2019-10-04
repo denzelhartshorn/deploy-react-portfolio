43372f71f00b8efb31f7807f67a71973e0d19f00
Denzel Hartshorn <denzelhartshorn@gmail.com>
2019-10-04 15:18:37 -0700

    make it better

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-if (process.env.NODE_EVN === "production") {
-  app.use(express.static("deployed-portfolio/build"));
+// if (process.env.NODE_EVN === "production") {
+//   app.use(express.static("deployed-portfolio/build"));
 
-  app.getI("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   app.getI("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 const PORT = process.env.PORT || 3000;
 serviceWorker.unregister();
