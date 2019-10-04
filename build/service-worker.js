96e7de59bbbc9e1a7ce24ad3c8609d1fb66b62ab
Denzel Hartshorn <denzelhartshorn@gmail.com>
2019-10-04 13:37:56 -0700

    starting heroku deploy

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React-Portfolio/precache-manifest.3586cabd51c7e6161b6c49e2945bb7ea.js"
+  "/deploy-react-portfolio/precache-manifest.e2e1c6e98df9ab2263c9e0e77321b74d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/React-Portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/deploy-react-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
