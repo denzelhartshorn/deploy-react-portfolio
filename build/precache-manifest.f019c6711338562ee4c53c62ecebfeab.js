cfab0bed266fc79a5ed42bcd842419697da6f249
Denzel Hartshorn <denzelhartshorn@gmail.com>
2020-08-03 12:53:20 -0700

    working, need to update

---
+++ build/precache-manifest.f019c6711338562ee4c53c62ecebfeab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bc76351fbd490b20c04",
+    "url": "/deploy-react-portfolio/static/css/main.21eabb69.chunk.css"
+  },
+  {
+    "revision": "5bc76351fbd490b20c04",
+    "url": "/deploy-react-portfolio/static/js/main.5bc76351.chunk.js"
+  },
+  {
+    "revision": "6221052083d9d7c06dcf",
+    "url": "/deploy-react-portfolio/static/js/1.62210520.chunk.js"
+  },
+  {
+    "revision": "728eb2de50637accaa50",
+    "url": "/deploy-react-portfolio/static/js/runtime~main.728eb2de.js"
+  },
+  {
+    "revision": "0ef64b30feea684be753a11af2daf760",
+    "url": "/deploy-react-portfolio/index.html"
+  }
+];
